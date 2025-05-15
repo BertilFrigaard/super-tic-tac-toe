54115f74fc409ee0ef7b369cd569567634290aa6
BertilF <bertilfrigaard@gmail.com>
2025-05-15 15:11:05 +0200

    Switched to own react setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import HomePage from "./pages/HomePage";
+import OfflineGamePage from "./pages/OfflineGamePage";
+
+function App() {
+    return (
+        <OfflineGamePage />
+    )
+}
+
+export default App;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,13 @@
 function HomePage() {
     return (
-        <footer>
-            <p>Hej, mit navn er William jeg har lavet denne ting fordi jeg er så klog at jeg kan programmere :)</p>
-        </footer>
+        <>
+            <header>
+                <h1>Super Tic-Tac-Toe</h1>
+            </header>
+            <footer>
+                <p>Hej, mit navn er William jeg har lavet denne ting fordi jeg er så klog at jeg kan programmere :)</p>
+            </footer>
+        </>
     )
 }
 
